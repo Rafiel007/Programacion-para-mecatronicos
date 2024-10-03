3673273b157b68322718c77d2297f9f70af6487e
tortoise ray <Mariaclaro047@gmail.com>
2024-10-03 12:58:55 -0400

    1

---
+++ 4/4.1/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int cubo(void);         //prototipo funcion//
+
+int I;                  //variable global//
+
+void main(void)
+{
+    int CUB;
+    for (I=1; I <= 10; I++)
+    {
+
+
+        CUB= cubo ();       //llamada a la funcion cubo//
+    printf("\nEl cubo de %d es:  %d , I, CUB" );
+    }
+}
+
+int cubo(void)              //declaracion de la funcion//
+//la funcion calcula el cubo de la vcariable global I. //
+{
+    return (I*I*I);
+}
