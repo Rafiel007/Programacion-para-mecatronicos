c77c8cf4698e59601ca122f6918872e9d308bec0
tortoise ray <Mariaclaro047@gmail.com>
2024-10-03 23:30:07 -0400

    listo

---
+++ examen/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+ void main(void)
+{
+float C (float);
+float G = 3.786;
+float Litros = 8.0;
+
+for (Litros; Litros>=7 & Litros <=12; Litros++);
+{
+
+    printf("\nCuantos litros que consume por galon entre 8 y 12 son %f=%f\n",Litros,C);
+
+
+    float Litros;
+    float G = 3.786;
+    float C (float);
+
+
+}
+
+
+
+if (Litros<7)
+{
+    printf ("\n solo se imprimen numeros del 8 hasta el 12");
+}
+if (Litros>12)
+{
+    printf("solo se imprimen numero del 8 hasta el 12");
+}
+
+
+
+ return Litros = C;Litros/100 ;Litros/G; Litros*290;
+
+}
